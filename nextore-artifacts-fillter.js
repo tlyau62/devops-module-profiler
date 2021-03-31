98b4349d44152405aec8acfb6f6a1c46ee28814e
Tsz Lam <tyautl@nexify.com.hk>
2021-03-31 12:25:20 +0800

    Modularise

--- nextore-artifacts-fillter.js
+++ nextore-artifacts-fillter.js
@@ -11,105 +11,103 @@
 // @grant        none
 // ==/UserScript==
 
-const mapping = {
-  "BD-EPS-Frontend": "",
-  ECMCommon: "NexTore.ECM.Common",
-  ECMECF: "NexTore.ECM.ECF",
-  ECMeCounter: "NexTore.ECM.eCounter",
-  ECMeDirectory: "NexTore.ECM.eDirectory",
-  ECMPlanViewer: "NexTore.ECM.PlanViewer",
-  EPSeProcessing: "NexTore.EPS.eProcessing",
-  ESHRegistration: "NexTore.ESH.Registration",
-  ESSeCounter: "NexTore.ESS.eCounter",
-  FormToPdf: "",
-  NexToreV3Repo: "",
-  "Viewer.Pas": "NexTore.Viewer.Pas",
-};
+jQuery.noConflict();
 
-const artifactFiller = () =>
-  $(".ms-List-surface:first .ms-List-page > [role=presentation]")
-    .toArray()
-    .map((artifact) => ({
-      alias: $(artifact).find(".flat-view-text-preserve").text(),
-      tfsEnhancements: $(artifact).find('.combo[id^="vss_"]').data()
-        .tfsEnhancements,
-      option() {
-        return this.tfsEnhancements[0];
+(function ($) {
+  "use strict";
+
+  const saver = (function () {
+    let artifacts = [];
+
+    const artifactList = () =>
+      $(
+        ".release-summary-node-artifacts-list .release-summary-node-artifact-view"
+      ).toArray();
+
+    const mapArtifacts = (el) => ({
+      artifact: $(el),
+      alias() {
+        return this.artifact.find(
+          ".release-summary-node-artifact-alias-text .release-summary-node-text-element"
+        )[0].childNodes[0].nodeValue;
       },
-      dataSource() {
-        return this.option()._behavior._dataSource._source;
+      version() {
+        return this.artifact
+          .find(".release-summary-node-artifact-version-text a")
+          .text();
       },
-      dataSourceDetails() {
-        return this.dataSource()
-          .map((dataSource) => ({
-            dataSource,
-            parts: dataSource.split("_"),
-          }))
-          .map((dataSource) => ({
-            dataSource: dataSource.dataSource,
-            version: dataSource.parts[2],
-            alias: dataSource.parts[0],
-            branch: dataSource.parts[1],
-          }));
+      branch() {
+        return this.artifact.find(
+          ".release-summary-node-artifact-branch-section .release-summary-node-text-element"
+        )[0].childNodes[0].nodeValue;
       },
-      input() {
-        return $(this.option()._input);
-      },
-      setInput(val) {
-        this.input().val(val);
-        this.input().trigger("change");
-      },
-      latestVersion() {
-        const map = mapping[this.alias];
-        const dataSource = this.dataSourceDetails();
-        const target = dataSource.find((d) => d.alias === map) || dataSource[0];
+    });
 
-        return target.dataSource;
-      },
-      setVersion() {
-        this.setInput(this.latestVersion());
-      },
-      loadVersion() {
-        const targetArtifact = artifacts.find(
-          (artifact) => artifact.alias() === this.alias
-        );
+    const extractArtifacts = () => artifactList().map(mapArtifacts);
 
-        this.setInput(targetArtifact.version());
-      },
-    }));
+    const saveArtifacts = () => (artifacts = extractArtifacts());
 
-window.filler = () =>
-  artifactFiller().forEach((artifact) => artifact.setVersion());
+    return {
+      saveArtifacts,
+      artifacts: () => artifacts,
+    };
+  })();
 
-window.fillerCurrentVersion = () =>
-  artifactFiller().forEach((artifact) => artifact.loadVersion());
+  const loader = (function () {
+    const artifactFiller = () =>
+      $(".ms-List-surface:first .ms-List-page > [role=presentation]")
+        .toArray()
+        .map((artifact) => ({
+          alias: $(artifact).find(".flat-view-text-preserve").text(),
+          tfsEnhancements: $(artifact).find('.combo[id^="vss_"]').data()
+            .tfsEnhancements,
+          option() {
+            return this.tfsEnhancements[0];
+          },
+          dataSource() {
+            return this.option()._behavior._dataSource._source;
+          },
+          dataSourceDetails() {
+            return this.dataSource()
+              .map((dataSource) => ({
+                dataSource,
+                parts: dataSource.split("_"),
+              }))
+              .map((dataSource) => ({
+                dataSource: dataSource.dataSource,
+                version: dataSource.parts[2],
+                alias: dataSource.parts[0],
+                branch: dataSource.parts[1],
+              }));
+          },
+          input() {
+            return $(this.option()._input);
+          },
+          setInput(val) {
+            this.input().val(val);
+            this.input().trigger("change");
+          },
+          loadVersion(artifacts) {
+            const targetArtifact = artifacts.find(
+              (artifact) => artifact.alias() === this.alias
+            );
 
-const artifactList = () =>
-  $(
-    ".release-summary-node-artifacts-list .release-summary-node-artifact-view"
-  ).toArray();
+            this.setInput(targetArtifact.version());
+          },
+        }));
 
-const mapArtifacts = (el) => ({
-  artifact: $(el),
-  alias() {
-    return this.artifact.find(
-      ".release-summary-node-artifact-alias-text .release-summary-node-text-element"
-    )[0].childNodes[0].nodeValue;
-  },
-  version() {
-    return this.artifact
-      .find(".release-summary-node-artifact-version-text a")
-      .text();
-  },
-  branch() {
-    return this.artifact.find(
-      ".release-summary-node-artifact-branch-section .release-summary-node-text-element"
-    )[0].childNodes[0].nodeValue;
-  },
-});
+    const fillLastVersion = (artifacts) =>
+      artifactFiller().forEach((artifact) => artifact.loadVersion(artifacts));
 
-const extractArtifacts = () => artifactList().map(mapArtifacts);
+    return {
+      fillLastVersion: () => fillLastVersion(saver.artifacts()),
+    };
+  })();
 
-window.saveArtifacts = () => (window.artifacts = extractArtifacts());
+  window.nextoreArtifacts = {
+    saver,
+    loader,
+  };
 
-console.log("Keep smile");
+  console.log("Keep smile");
+})($);
